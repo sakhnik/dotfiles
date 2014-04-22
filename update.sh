f310b2b862ea6b42d222f06b4085016febb1888e
Anatolii Sakhnik <anatolii.sakhnik@lge.com>
2014-04-22 11:34:37 +0300

    Update vim plugins

--- update.sh
+++ update.sh
@@ -10,18 +10,21 @@ updated=
 head_rev=$(git show-ref refs/heads/master | cut -f1 -d' ')
 git pull --rebase
 new_head_rev=$(git show-ref refs/heads/master | cut -f1 -d' ')
-[[ "$head_ref" != "$new_head_rev" ]] && updated=1
 
-git submodule foreach git pull origin master
-LANG=C git status --porcelain | grep -q '^ [MDA]'
-if [[ $? -eq 0 ]]; then
+if [[ "$head_ref" == "$new_head_rev" ]]; then
+	git submodule foreach git pull origin master
+	LANG=C git status --porcelain | grep -q '^ [MDA]'
+	if [[ $? -eq 0 ]]; then
+		updated=1
+		git commit -a -m 'Update vim plugins'
+	fi
+else
 	updated=1
-	git commit -a -m 'Update vim plugins'
 fi
 
-git submodule update --init --recursive
-
 if [[ -n "$updated" ]]; then
+	git submodule update --init --recursive
+
 	echo "Compile vimproc"
 	pushd .vim/bundle/vimproc
 	make
