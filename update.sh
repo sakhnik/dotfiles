4dcf474684bcabdf2020823068a230bc92948d0d
Anatolii Sakhnik <anatolii.sakhnik@lge.com>
2014-11-02 20:43:20 +0200

    Fix updating ycm

--- update.sh
+++ update.sh
@@ -25,7 +25,7 @@ fi
 if [[ -n "$updated" ]]; then
 	git submodule update --init --recursive
 
-	if ! git diff --quiet HEAD^.. .vim/bundle/ycm >/dev/null 2>&1; then
+	if ! git diff --quiet $head_rev.. .vim/bundle/ycm >/dev/null 2>&1; then
 		echo "Compile ycm"
 		pushd .vim/bundle/ycm
 		./install.sh --clang-completer --system-libclang
