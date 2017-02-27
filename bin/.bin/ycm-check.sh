c842e45e8c4479e1294e4d4673b65c08ac14021d
Anatolii Sakhnik <sakhnik@gmail.com>
2017-02-27 11:24:46 +0200

    Check YCM only if it was deployed

--- bin/.bin/ycm-check.sh
+++ bin/.bin/ycm-check.sh
@@ -4,9 +4,15 @@ set -e
 
 # Check that all directories in includes.txt are still present
 # If something is missing, high chance that stdc++ has been updated.
-for i in `cat ~/.vim/includes.txt`; do
-    [[ -d $i ]] || exit 1
-done
+includes=~/.vim/includes.txt
+if [[ -f $includes ]]; then
+	for i in `cat $includes`; do
+		[[ -d $i ]] || exit 1
+	done
+fi
 
 # Check that system libraries haven't changed for ycmd
-ldd ~/.vim/YouCompleteMe/third_party/ycmd/ycm_core.so | grep -q 'not found' && exit 2 || true
+library=~/.vim/YouCompleteMe/third_party/ycmd/ycm_core.so
+if [[ -f $library ]]; then
+	ldd $library | grep -q 'not found' && exit 2 || true
+fi
