02415e26b79e62d2ec067120928c59e92af170ea
Anatolii Sakhnik <sakhnik@gmail.com>
2017-09-04 22:16:45 +0200

    Allow deploying to a custom location

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+dstdir=${1:-~}
+
 dotfiles=`readlink -f $(dirname ${BASH_SOURCE[0]})`
 cd "$dotfiles"
 
 # Utilize GNU stow to symlink from HOME to our config files.
 for i in *; do
-	[[ -d $i ]] && stow $i
+	[[ -d $i ]] && stow -t "$dstdir" $i
 done
