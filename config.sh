7c1b1b753f9f24c865ea7da185a1fd2af8877667
Anatolii Sakhnik <sakhnik@gmail.com>
2017-09-06 20:29:09 +0200

    Test and fix vim plugin installation

--- config.sh
+++ config.sh
@@ -5,9 +5,8 @@ set -e
 export this_dir=$(dirname `readlink -f ${BASH_SOURCE[0]}`)
 cd $this_dir
 
-zsh -df <<END
 export HOME=$this_dir/src
-source "src/.zshrc"
-ycm-update.sh
+
+zsh -df -c "source src/.zshrc && ycm-update.sh"
+
 vim +PlugInstall +qa
-END
