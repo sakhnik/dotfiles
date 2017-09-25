7d3b6765bb4e0763fe85129d30950926231236e2
Anatolii Sakhnik <sakhnik@gmail.com>
2017-09-25 22:01:50 +0300

    Fix deployment when src is dirty

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ set -e
 dotfiles=`readlink -f $(dirname ${BASH_SOURCE[0]})`
 cd "$dotfiles"
 
+ignores=$(git status --porcelain src | perl -ne 'if (m/^?? src\/(.*)/) { print $1; if (!eof) {print "|"}}')
+
 # Utilize GNU stow to symlink from HOME to our config files.
-stow -Svt $HOME src
+stow -Svt $HOME --ignore "($ignores)" src
