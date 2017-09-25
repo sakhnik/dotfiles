d5e4f6ab93900e2c8aa90d09063b0e2653640b05
Anatolii Sakhnik <sakhnik@gmail.com>
2017-09-25 22:23:11 +0300

    Handle dotfile in ignore correctly

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ set -e
 dotfiles=`readlink -f $(dirname ${BASH_SOURCE[0]})`
 cd "$dotfiles"
 
-ignores=$(git status --porcelain src | perl -ne 'if (m/^?? src\/(.*)/) { print $1; if (!eof) {print "|"}}')
+ignores=$(git status --porcelain src | \
+	perl -ne 'if (m/^?? src\/(.*)/) {$a=$1; $a=~s/\./\\./g; print $a; if(!eof){print "|"}}')
 
 # Utilize GNU stow to symlink from HOME to our config files.
 stow -Svt $HOME --ignore "($ignores)" src
