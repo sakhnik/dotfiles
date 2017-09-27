92eac0fa73560f0a905489148628ed25f82927fd
Anatolii Sakhnik <sakhnik@gmail.com>
2017-09-27 22:51:18 +0300

    Make deploy.sh independent from git ignore

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,16 @@ set -e
 dotfiles=`readlink -f $(dirname ${BASH_SOURCE[0]})`
 cd "$dotfiles"
 
-ignores=$(git status --porcelain src | \
-	perl -ne 'if (m/^?? src\/(.*)/) {$a=$1; $a=~s/\./\\./g; print $a; if(!eof){print "|"}}')
+
+ignores=$( \
+    comm -23 <(find src -maxdepth 1 -mindepth 1 | sort) \
+             <(git ls-files src | grep -oE 'src/[^/]+' | sort -u) \
+    | perl -ne 's/^src\/(.*)/\1/;
+                s/\n//;
+                s/\./\\./g;
+                print $_;
+                if(!eof){print "|"}' \
+)
 
 # Utilize GNU stow to symlink from HOME to our config files.
 stow -Svt $HOME --ignore "($ignores)" src
