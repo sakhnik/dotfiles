e414b4740cac713447865a960c0f523436d58ab6
Anatolii Sakhnik <sakhnik@gmail.com>
2017-10-14 23:05:09 +0300

    Prepare a generator for keystrokes to replay

--- test/replay.py
+++ test/replay.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
-import time, sys
+import time, os, sys
+import subprocess, pty
 
-with open("input", "rb") as f:
-    while f:
-        params = f.readline().split()
-        if not params:
-            break
-        delay = float(params[0])
-        duration = float(params[1])
-        count = int(params[2])
-        # 1. Delay
-        time.sleep(delay)
-        keystrokes = f.read(count)
-        d = duration / len(keystrokes)
-        for k in keystrokes:
-            sys.stdout.write("%c" % k)
-            sys.stdout.flush()
-            time.sleep(d)
-        f.read(1)    # newline
+def InputReader():
+    with open("input", "rb") as f:
+        while f:
+            params = f.readline().split()
+            if not params:
+                break
+            delay = float(params[0])
+            duration = float(params[1])
+            count = int(params[2])
+            keystrokes = f.read(count)
+            d = 0
+            if count > 1:
+                d = duration / (count - 1)
+            for k in keystrokes:
+                yield delay, k
+                delay = d
+            f.read(1)    # newline
+
+keys = InputReader()
+for d,k in keys:
+    time.sleep(d)
+    sys.stdout.write("%c" % k)
+    sys.stdout.flush()
