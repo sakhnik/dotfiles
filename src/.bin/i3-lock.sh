25a1ee17454529d2face3766952fbc8a989de8c1
Anatolii Sakhnik <sakhnik@gmail.com>
2017-12-02 22:33:18 +0200

    Try plain i3lock

--- src/.bin/i3-lock.sh
+++ src/.bin/i3-lock.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -e
 
 # Take a screenshot
-scrot /tmp/screen_locked.png
+#scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
 #mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-mogrify -blur 20x10 /tmp/screen_locked.png
+#mogrify -blur 0x16 /tmp/screen_locked.png
 
 # Ensure English keyboard to easy unlocking
 xkb-switch -s us
 
 # Lock screen displaying this image.
-i3lock -i /tmp/screen_locked.png
+i3lock -c 777777 #-i /tmp/screen_locked.png
 
 # Turn the screen off after a delay.
 sleep 60; pgrep i3lock && xset dpms force off
