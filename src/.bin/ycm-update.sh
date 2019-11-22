40cd8aaf134493e794b21e1bab56d513ddd84ff8
Anatolii Sakhnik <sakhnik@gmail.com>
2019-11-22 09:59:18 +0200

    Fix linter warnings

--- src/.bin/ycm-update.sh
+++ src/.bin/ycm-update.sh
@@ -5,21 +5,21 @@
 
 set -e
 
-vimdir=$(cd `dirname ${BASH_SOURCE[0]}`/../.vim; pwd)
+vimdir="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.vim; pwd)"
 
 # Arrange a temporary directory
-workdir=`mktemp -d /tmp/ycm.XXXXXXX`
+workdir=$(mktemp -d /tmp/ycm.XXXXXXX)
 trap 'rm -rf $workdir' EXIT
 
 # Clone the source code of the plugin
-cd $workdir
+cd "$workdir"
 git clone --depth 1 https://github.com/Valloric/YouCompleteMe
 cd YouCompleteMe
 git submodule update --init --recommend-shallow --jobs 2
 
 # Clone the rest of submodules
 git submodule update --init --recursive --recommend-shallow --jobs 8
-find -name '.git*' -prune -exec rm -rf {} \;
+find . -name '.git*' -prune -exec rm -rf {} \;
 
 # Build completion of C++ code
 python install.py --clang-completer --system-boost --system-libclang
@@ -35,11 +35,11 @@ find third_party -type d -name docs -prune -exec rm -rf {} \;
 
 # Deploy the plugin into vim directory
 cd ..
-rsync -raP --delete YouCompleteMe $vimdir/
+rsync -raP --delete YouCompleteMe "$vimdir/"
 
 # Update vim help tags
 vim="vim -es"
-if which nvim >/dev/null 2>&1; then
+if command -v nvim >/dev/null 2>&1; then
     vim="nvim -es --headless"
 fi
 
