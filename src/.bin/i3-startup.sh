c83b9e9365898978567bc0d83577fd38cd4ffd92
Anatolii Sakhnik <sakhnik@gmail.com>
2021-02-05 22:19:44 +0200

    Fix i3-startup.sh

--- src/.bin/i3-startup.sh
+++ src/.bin/i3-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 setxkbmap -layout us,ua -option grp:alt_space_toggle,lv3:ralt_switch,compose:rctrl-altgr -model chromebook
-xinput set-prop "Elan Touchpad" 308 1  # Tap to click
-xinput set-prop "Elan Touchpad" 316 1  # Natural scrolling
+xinput set-prop "Elan Touchpad" "libinput Tapping Enabled" 1  # Tap to click
+xinput set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1
