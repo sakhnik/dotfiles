444a8d84c0d3d0c72a80d09f56d0ba21649b80e8
Anatolii Sakhnik <sakhnik@gmail.com>
2023-08-21 20:46:10 +0300

    nvim: disable treesitter on bigger files

--- src/.config/nvim/lua/local/treesitter.lua
+++ src/.config/nvim/lua/local/treesitter.lua
@@ -18,11 +18,14 @@ function C.setup()
       -- `false` will disable the whole extension
       enable = true,
 
-      -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-      -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-      -- the name of the parser)
-      -- list of language that will be disabled
-      disable = { "c", "rust", "json" },
+      -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+      disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
 
       -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
